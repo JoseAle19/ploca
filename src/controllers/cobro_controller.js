37062389718f2eb996a7a991522ba0d4e79094c8
DevJose <devjose@email.com>
2024-05-19 22:08:08 -0600

    cobros

--- src/controllers/cobro_controller.js
+++ src/controllers/cobro_controller.js
@@ -8,12 +8,8 @@ export const getCobros = async (req, res) => {
 
     const result = await cobros.findAll();
     result.forEach(cli => {
-      // "fac_id": "RE-00216",
-      // "not_importe": 4475.657999999999,
-      // "cob_saldo_actual": 0.0010000000000331966,
-      // "nuevo_cobro": 0.000008912655971775371,
-      // "resultado_division": 0.000008912655971775371,
-      // "nombre cliente": "LOPEZ CANO MARIA DEL CARMEN"
+      delete cli.res;
+
     });
 
     res.json(result);
