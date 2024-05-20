1cad450778cbe7c4918b22fc9c5fdf121a8ff6e7
DevJose <devjose@email.com>
2024-05-19 22:06:35 -0600

    cobros

--- src/models/cobros.js
+++ src/models/cobros.js
@@ -9,11 +9,11 @@ export class CobrosModel {
         return new Promise((resolve, reject) => {
             const query = `SELECT 
  
-n.fac_id,
-n.not_importe,
-c.cob_saldo_actual,
- c.cob_saldo_actual / (n.not_importe / n.not_cantidad) AS 'nuevo_cobro',
- c.cob_saldo_actual / (n.not_importe / n.not_cantidad) AS resultado_division,
+n.fac_id as nota,
+n.not_importe as importe,
+c.cob_saldo_actual as pago ,
+ c.cob_saldo_actual / (n.not_importe / n.not_cantidad) AS 'KG pagados',
+ c.cob_saldo_actual / (n.not_importe / n.not_cantidad) AS res,
 cl.cli_nombre as 'nombre cliente'
 FROM 
 Plosa.notas n
@@ -21,7 +21,7 @@ JOIN
 Plosa.cobros c ON n.fac_id = c.fac_id
 join plosa.facturas f on n.fac_id = f.fac_id and c.fac_id = f.fac_id
 join plosa.clientes cl on f.cli_numero = cl.cli_numero 
-HAVING resultado_division != 0
+HAVING res != 0
 LIMIT 10`;
 
             connection.query(query, (err, result) => {
